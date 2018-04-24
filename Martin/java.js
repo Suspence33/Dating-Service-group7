fb99629fa850b8a52a717dce09b6191cb066bbfa
Wolferyus <35543538+Wolferyus@users.noreply.github.com>
2018-04-24 14:44:05 -0400

    Add files via upload

--- Martin/java.js
+++ Martin/java.js
@@ -1,4 +1,26 @@
-document.onload = alert("You must be of the legal age of 18 to use this site");
+document.onload = alert("You must be the legal age of 18 to use this site");
+function Free trial(){
+var text = document.getElementById ("")
 	
+}
 
+
+
+
+function Basic(){
 	
+var text = document.getElementById ("")
+	
+}
+
+function Premium(){
+	
+var text = document.getElementById ("")
+	
+}
+
+function Deluxe(){
+	
+var text = document.getElementById ("")
+	
+}
